b4bd773a3c4eb522ff10b5c24ed1f612b3377035
AyyoobAjward <ajwardayyoob@gmail.com>
2026-01-16 19:24:29 +0530

    Refactor logger configuration by removing unused net import and cleaning up comments

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,13 +1,12 @@
 import { WinstonModule } from 'nest-winston';
 import * as winston from 'winston';
-import * as net from 'net';
 
 export const logger = WinstonModule.createLogger({
   transports: [
     new winston.transports.Console(),
 
     new winston.transports.Http({
-      host: 'localhost',   // docker host
+      host: 'localhost',
       port: 8080,
       path: '/',
       ssl: false,
